fd1e88c80d5f7e134dc08a7c3e3909a8a268e32b
Armando Esparza <armando_eg13@hotmail.com>
2026-09-03 13:53:38 -0600

    release: 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "com.ares.ewe_man"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
